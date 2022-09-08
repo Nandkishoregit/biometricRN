bd06a8fbea7faa0c75e544fa60793f4c7f860a6d
Nandkishoregit <107661833+Nandkishoregit@users.noreply.github.com>
2022-09-08 12:13:01 -0400

    React Native Biometric POC

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as LocalAuthentication from 'expo-local-authentication';
 
+//Auth for TouchID and FaceID on iOS and Android devices
 export default function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function authenticate() {
+    const result = await LocalAuthentication.authenticateAsync();
+    //can handle error cases if biometric fails by destructuring result
+    console.log('result', result);
+    setIsAuthenticated(result.success);
+  }
+
+  useEffect(() => {
+    (async () => {
+      // Check if hardware supports biometrics
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      //check if the user is enrolled in biometrics on the device
+      const savedBiometrics = await LocalAuthentication.isEnrolledAsync();
+
+      if (compatible && savedBiometrics) {
+        authenticate();
+      }
+      if (!savedBiometrics)
+        alert('Biometric record not found, use default login method to authenticate');
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{isAuthenticated ? "You are Authenicated" : "Biometric failed, Use your username and password to login"}</Text>
     </View>
   );
 }
@@ -18,3 +44,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
